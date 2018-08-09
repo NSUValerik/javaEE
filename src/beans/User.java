f97745368437ba975a31aaba4df09850f1bb3cc2
nsuvalerik <nsuvalerik@developmentontheedge.com>
2018-08-09 12:47:56 +0700

    new branch commit

--- src/beans/User.java
+++ src/beans/User.java
@@ -1,4 +1,23 @@
 package beans;
 
 public class User {
+    private Person person;
+
+    private String param;
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public String getParam() {
+        return param;
+    }
+
+    public void setParam(String param) {
+        this.param = param;
+    }
 }
